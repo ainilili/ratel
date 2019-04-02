08b50020888823a6941a7980369a893bc7e047f2
DoyuTu <859898972@qq.com>
2019-04-03 01:27:08 +0800

    use epoll event loop

--- landlords-server/src/main/java/org/nico/ratel/landlords/server/SimpleServer.java
+++ landlords-server/src/main/java/org/nico/ratel/landlords/server/SimpleServer.java
@@ -1,41 +1,41 @@
 package org.nico.ratel.landlords.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import java.net.InetSocketAddress;
 import java.util.Timer;
-
 import org.nico.ratel.landlords.print.SimplePrinter;
 import org.nico.ratel.landlords.server.handler.DefaultChannelInitializer;
 import org.nico.ratel.landlords.server.timer.RoomClearTask;
 
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-
 public class SimpleServer {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		if(args != null && args.length > 1) {
 			if(args[0].equalsIgnoreCase("-p") || args[0].equalsIgnoreCase("-port")) {
 				ServerContains.port = Integer.valueOf(args[1]);
 			}
 		}
-		
-		EventLoopGroup parentGroup = new NioEventLoopGroup();
-		EventLoopGroup childGroup = new NioEventLoopGroup();
+
+		EventLoopGroup parentGroup = Epoll.isAvailable() ? new EpollEventLoopGroup() : new NioEventLoopGroup();
+		EventLoopGroup childGroup = Epoll.isAvailable() ? new EpollEventLoopGroup() : new NioEventLoopGroup();
 		try {
 			ServerBootstrap bootstrap = new ServerBootstrap()
 			.group(parentGroup, childGroup)
 			.channel(NioServerSocketChannel.class)
 			.localAddress(new InetSocketAddress(ServerContains.port))
 			.childHandler(new DefaultChannelInitializer());
-			
+
 			ChannelFuture f = bootstrap .bind().sync();
-			
+
 			SimplePrinter.serverLog("The server was successfully started on port " + ServerContains.port);
-			
+
 			ServerContains.THREAD_EXCUTER.execute(() -> {
 				Timer timer=new Timer();
 				timer.schedule(new RoomClearTask(), 0L, 3000L);
@@ -45,7 +45,7 @@ public class SimpleServer {
 			parentGroup.shutdownGracefully();
 			childGroup.shutdownGracefully();
 		}
-		
-		
+
+
 	}
 }
