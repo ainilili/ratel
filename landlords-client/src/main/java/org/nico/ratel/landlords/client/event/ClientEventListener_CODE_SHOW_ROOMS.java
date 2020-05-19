49b7edef7f1e929bbeea6eb4c587726c3087f3a5
zfh <fuhongzuo@gmail.com>
2020-05-19 17:08:36 +0800

    Fix #49

--- landlords-client/src/main/java/org/nico/ratel/landlords/client/event/ClientEventListener_CODE_SHOW_ROOMS.java
+++ landlords-client/src/main/java/org/nico/ratel/landlords/client/event/ClientEventListener_CODE_SHOW_ROOMS.java
@@ -20,7 +20,7 @@ public class ClientEventListener_CODE_SHOW_ROOMS extends ClientEventListener{
 		List<Map<String, Object>> roomList = Noson.convert(data, new NoType<List<Map<String, Object>>>() {});
 		if(roomList != null && ! roomList.isEmpty()){
 			// "COUNT" begins after NICKNAME_MAX_LENGTH characters. The dash means that the string is left-justified.
-			String format = "#\t%s\t|\t%-" + NICKNAME_MAX_LENGTH + "s\t|\t%s\t|\t%s\t#\n";
+			String format = "#\t%s\t|\t%-" + NICKNAME_MAX_LENGTH + "s\t|\t%-6s\t|\t%-6s\t#\n";
 			FormatPrinter.printNotice(format, "ID", "OWNER", "COUNT", "TYPE");
 			for(Map<String, Object> room: roomList) {
 				FormatPrinter.printNotice(format, room.get("roomId"), room.get("roomOwner"), room.get("roomClientCount"), room.get("roomType"));
