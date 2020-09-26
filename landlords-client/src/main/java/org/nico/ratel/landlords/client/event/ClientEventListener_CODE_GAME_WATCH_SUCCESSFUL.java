b26d5482a78e28651c0626674716a5e4d3f7c2c0
zxw <zhangxunweia@gmail.com>
2020-09-26 20:41:21 +0800

    新增此类用于接受观战成功事件

---
+++ landlords-client/src/main/java/org/nico/ratel/landlords/client/event/ClientEventListener_CODE_GAME_WATCH_SUCCESSFUL.java
@@ -0,0 +1,28 @@
+package org.nico.ratel.landlords.client.event;
+
+import io.netty.channel.Channel;
+import org.nico.ratel.landlords.client.entity.User;
+import org.nico.ratel.landlords.helper.MapHelper;
+import org.nico.ratel.landlords.print.SimplePrinter;
+
+import java.util.Map;
+
+public class ClientEventListener_CODE_GAME_WATCH_SUCCESSFUL extends ClientEventListener {
+
+    private static final String WATCH_SUCCESSFUL_TIPS = "                                                 \n" +
+                                                        "+------------------------------------------------\n" +
+                                                        "|You are already watch the game.         \n"         +
+                                                        "|Room owner: %s. Room current status: %s.\n"         +
+                                                        "+------------------------------------------------\n" +
+                                                        "                                                   ";
+
+    @Override
+    public void call(Channel channel, String data) {
+        // 修改User.isWatching状态
+        User.INSTANCE.setWatching(true);
+
+        // 进入观战提示
+        Map<String, Object> map = MapHelper.parser(data);
+        SimplePrinter.printNotice(String.format(WATCH_SUCCESSFUL_TIPS, map.get("owner"), map.get("status")));
+    }
+}
